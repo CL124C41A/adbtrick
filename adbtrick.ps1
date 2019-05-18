6a5027ad8d0ab457b4cf5b585311799b4deacb8c
Claudio Venanzi <cl124c41a@gmail.com>
2019-05-18 14:08:24 +0200

    Modularizing the script
    Added proper arguments, moved list in a separate file, etc.

--- adbtrick.ps1
+++ adbtrick.ps1
@@ -1,138 +1,69 @@
-
-#######################
-# BASIC CONFIGURATION #
-#######################
-# (e)xecutables - paths to executables
-# (d)irectories - paths with trailing '\'
-# (f)lags       - $true | $false
-
-$binary_e = ".\platform-tools\adb.exe"
-$backup_d = ".\backup\"
-$backup_f = $true
-$remove_f = $true
-
-##########################
-# ADVANCED CONFIGURATION #
-##########################
-# A complete reference of all the software present by default can be found
-# in the companion file: /data/WAS-LX1A_SW.txt
-
-$selection = @(
-
-	# GAMES
-	,"package:/data/hw_init/version/special_cust/WAS-L01A/hw/eu/app/SpiderManUltimatePower/SpiderManUltimatePower.apk=com.gameloft.android.GloftSMIF"
-	,"package:/data/hw_init/version/special_cust/WAS-L01A/hw/eu/app/DisneyMagicKingdom/DisneyMagicKingdom.apk=com.gameloft.android.GloftDMKF"
-	,"package:/data/hw_init/version/special_cust/WAS-L01A/hw/eu/app/PGA_ScratchCard/PGA_ScratchCard.apk=com.gameloft.android.GloftSCCA"
-	,"package:/data/hw_init/version/special_cust/WAS-L01A/hw/eu/app/AsphaltNitro/AsphaltNitro.apk=com.gameloft.android.GloftANPH"
-
-	# INSTAGRAM
-	,"package:/data/hw_init/version/special_cust/WAS-L01A/hw/eu/app/Instagram_Stub/Instagram_Stub.apk=com.instagram.android"
+<#
+.SYNOPSIS
+	Script used to remove unwanted applications from Android smartphones.
+.DESCRIPTION
+    Ever wanted to remove some system apps from your smartphone?
 	
-	# FACEBOOK
-	,"package:/data/hw_init/version/special_cust/WAS-L01A/hw/eu/app/Facebook_Services/Facebook_Services.apk=com.facebook.services"
-	,"package:/data/hw_init/version/special_cust/WAS-L01A/hw/eu/app/Facebook_Stub/Facebook_Stub.apk=com.facebook.katana"
-	,"package:/data/hw_init/version/special_cust/WAS-L01A/hw/eu/app/Appmanager/Appmanager.apk=com.facebook.appmanager"
-	,"package:/data/hw_init/version/special_cust/WAS-L01A/hw/eu/app/Installer/Installer.apk=com.facebook.system"
+	The web it's surely full of partial or overly specific guides;
+	with this script I'm trying to provide a simple way to perform
+	this operation in the simplest and safest way possible:
 	
-	# TWITTER
-	,"package:/data/hw_init/version/special_cust/WAS-L01A/hw/eu/app/Twitter/Twitter.apk=com.twitter.android"
+	* No root required
+	Packages are removed exclusively from the current user.
 	
-	# HUAWEI NOTEPAD
-	,"package:/system/priv-app/HwNotePad/HwNotePad.apk=com.example.android.notepad"
-
-	# HUAWEI CLONE
-	,"package:/system/delapp/PhoneClone_OVE/PhoneClone_OVE.apk=com.hicloud.android.clone"
-
-	# HUAWEI MUSIC
-	,"package:/system/priv-app/HwMediaCenter/HwMediaCenter.apk=com.android.mediacenter"
+	* Backups by default
+	The packages to be removed are pulled into a local folder.
 	
-	# HUAWEI MIRROR
-	,"package:/system/delapp/HwMirror/HwMirror.apk=com.android.hwmirror"
-	
-	# HUAWEI APPGALLERY
-	,"package:/data/hw_init/version/special_cust/WAS-L01A/hw/eu/app/HiSpace/HiSpace.apk=com.huawei.appmarket"
-	
-	# HUAWEI THEME MANAGER
-	,"package:/system/app/HwThemeManager/HwThemeManager.apk=com.huawei.android.thememanager"
+	* Configurable behaviour
+	Most of it can be changed with parameter flags.
+.PARAMETER fileSelect
+    Mandatory. Used to select the packages to operate on.
+	Expects a file of packages with one package per line.
+.PARAMETER pathBackup
+	Optional. .\backup\ by default.
+	Used to select a folder to put the pulled packages in.
+.PARAMETER flagBackup
+	Optional. $true dy default.
+	Used to enable the package backup before removal.
+.PARAMETER flagRemove
+	Optional. $true dy default.
+	Used to enable the package backup before removal.
+.EXAMPLE
+	Backups the packages defined by games.txt into the
+	./backup folder and remove them from the device.
 
-	# HUAWEI VIDEO
-	,"package:/system/priv-app/HiMovie/HiMovie.apk=com.huawei.himovie.overseas"
-	
-	# HUAWEI HEALTH
-	,"package:/data/hw_init/version/region_comm/oversea/app/HwHealthPlatform_OVE/HwHealthPlatform_OVE.apk=com.huawei.health"
+    adbtrick -fileSelect .\data\WAS-LX1A\games.txt
+.EXAMPLE
+	Backups the packages defined by aaa.txt
+	file into the .\bbb\ folder and stops.
 
-	# HUAWEI GAME SUITE
-	,"package:/data/hw_init/product/app/HwGameAssistant/HwGameAssistant.apk=com.huawei.gameassistant"	
+	adbtrick -fileSelect .\aaa.txt -pathBackup .\bbb\ -flagRemove $false
+.NOTES
+    Author: Claudio Venanzi
+    Date:   May 18, 2019
+#>
 
-	# HUAWEI TIPS
-	,"package:/system/app/HwSmartSuggestion/HwSmartSuggestion.apk=com.huawei.tips"
-	,"package:/system/delapp/HwEmuiManual/HwEmuiManual.apk=com.huawei.android.tips"
-
-	# HUAWEI COMPASS
-	,"package:/system/delapp/HwCompass/HwCompass.apk=com.huawei.compass"
-
-	# HUAWEI BACKUP
-	,"package:/system/delapp/HwBackup_Local/HwBackup_Local.apk=com.huawei.KoBackup"
-
-	# HUAWEI WEATHER
-	,"package:/system/priv-app/HwWeatherClock/HwWeatherClock.apk=com.huawei.android.totemweather"
-	,"package:/system/priv-app/HwWeatherClockApp/HwWeatherClockApp.apk=com.huawei.android.totemweatherapp"
-	,"package:/system/priv-app/HwWeatherClockWidget/HwWeatherClockWidget.apk=com.huawei.android.totemweatherwidget"
-
-	# HUAWEI ONLINE FOLDER
-	,"package:/cust/hw/eu/app/HiFolder/HiFolder.apk=com.huawei.hifolder"
-
-	# HUAWEI ONLINE ID
-	,"package:/cust/hw/eu/app/HMS/HMS.apk=com.huawei.hwid"
-
-	#--------------------------------------------------------------------------
-	
-	# GOOGLE SEARCH WIDGET
-	,"package:/system/priv-app/Velvet/Velvet.apk=com.google.android.googlequicksearchbox"
-
-	# GOOGLE PLAY MUSIC
-	,"package:/system/app/Music2/Music2.apk=com.google.android.music"
-
-	# GOOGLE PLAY FILM
-	,"package:/system/app/Videos/Videos.apk=com.google.android.videos"
-
-	# GOOGLE PHOTO
-	,"package:/system/app/Photos/Photos.apk=com.google.android.apps.photos"
-
-	# GOOGLE DUO
-	,"package:/system/app/Duo/Duo.apk=com.google.android.apps.tachyon"
-	
-	# GOOGLE EDIT DOCS
-	,"package:/data/hw_init/version/special_cust/WAS-L01A/hw/eu/app/EditorsDocs/EditorsDocs.apk=com.google.android.apps.docs.editors.docs"
-
-	# GOOGLE EDIT SHEETS
-	,"package:/data/hw_init/version/special_cust/WAS-L01A/hw/eu/app/EditorsSheets/EditorsSheets.apk=com.google.android.apps.docs.editors.sheets"
-	
-	# GOOGLE EDIT SLIDES
-	,"package:/data/hw_init/version/special_cust/WAS-L01A/hw/eu/app/EditorsSlides/EditorsSlides.apk=com.google.android.apps.docs.editors.slides"     
+param
+	([Parameter(Mandatory=$true)][string] $fileSelect
+	,[Parameter(Mandatory=$false)][string]$pathBackup = ".\backup\"
+	,[Parameter(Mandatory=$false)][bool]  $flagBackup = $true
+	,[Parameter(Mandatory=$false)][bool]  $flagRemove = $true
 )
 
-#################
-# PROGRAM LOGIC #
-#################
-# Here be dragons.
+# retrieve installed packages from device
+$ondevice = ((& adb shell 'pm list packages -f' | sort) -split " ")
 
-$on_device = ((& $binary_e shell 'pm list packages -f' | sort) -split " ")
+# setup the backup directory
+#if($flagBackup) {New-Item -ItemType Directory -Force -Path $pathBackup | Out-Null
 
-New-Item -ItemType Directory -Force -Path $backup_d | Out-Null
-
-ForEach ($package In $selection) {
-	if($on_device -contains $package) {
-		Write-Host ("`r`nPackage: "+$package);
-		
-		# PACKAGE SETUP
+ForEach($package in Get-Content $fileSelect) {
+	if($ondevice -contains $package) {
+		Write-Host ("Checking "+$package);
 		$splitted = ($package.Replace("package:", "") -split ".apk=")
-		
-		# BACKUP STEP
-		if($backup_f) {& $binary_e pull ($splitted[0]+".apk") $backup_d}
-	
-		# DELETE STEP
-		if($remove_f) {& $binary_e shell ('pm uninstall -k --user 0 '+$splitted[1])}		
+		if($backup_f) {& adb pull ($splitted[0]+".apk") $backup_d}	
+		if($remove_f) {& adb shell ('pm uninstall -k --user 0 '+$splitted[1])}		
+	}else{
+		Write-Host ("Skipping "+$package);
 	}
 }
 
